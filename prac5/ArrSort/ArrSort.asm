1c84423a14db03670dd7c4436a4c2f548e50d806
HoffmanEl <elijah.hoffman64@gmail.com>
2024-09-14 16:32:21 +0930

    Commit message

--- prac5/ArrSort/ArrSort.asm
+++ prac5/ArrSort/ArrSort.asm
@@ -1,52 +1,71 @@
-// Bubble Sort Implementation
+// Improved Bubble Sort Implementation
 // R1: Base address of the array
-// R2: Length of the array
+// R2: Length of the array (represented as negative value)
 // R0: Will be set to -1 when sorting is complete
 
-// Initialize outer loop counter
+// Initialize
 @R2
 D=M
-@n
-M=D      // n = length of array
+@length
+M=D      // length = R2 (negative value)
 
-(OUTER_LOOP)
-@n
+// Check if array is empty or has one element
+@length
 D=M
 @END
-D;JLE    // If n <= 1, end sorting (array is sorted)
+D;JGE    // If length >= 0, end sorting (array is empty or has one element)
+
+// Initialize outer loop counter
+@outerCounter
+M=-1     // Start from -1 to handle length stored as negative
+
+(OUTER_LOOP)
+@length
+D=M
+@outerCounter
+D=D-M
+@END
+D;JEQ    // If outerCounter == |length|, end sorting
 
 // Initialize inner loop counter and swap flag
-@i
-M=0      // i = 0
+@innerCounter
+M=0      // innerCounter = 0
 @swapped
 M=0      // swapped = false (0)
 
 (INNER_LOOP)
-@n
+@length
 D=M
-@i
+@innerCounter
+D=D+M    // D = length + innerCounter
+@outerCounter
 D=D-M
-D=D-1
+D=D+1
 @OUTER_LOOP_END
-D;JLE    // If i >= n-1, end inner loop
+D;JGE    // If innerCounter >= |length| + outerCounter + 1, end inner loop
 
 // Compare arr[i] and arr[i+1]
 @R1
 D=M
-@i
+@innerCounter
 A=D+M    // Address of arr[i]
 D=M      // D = arr[i]
-@temp
-M=D      // temp = arr[i]
+@temp1
+M=D      // temp1 = arr[i]
 
 @R1
 D=M
-@i
+@innerCounter
 D=D+M
 A=D+1    // Address of arr[i+1]
 D=M      // D = arr[i+1]
+@temp2
+M=D      // temp2 = arr[i+1]
 
-@temp
+// Compare using subtraction (works for both positive and negative numbers)
+@temp2
+D=M
+@temp1
 D=D-M    // D = arr[i+1] - arr[i]
 
 @SKIP_SWAP
@@ -55,50 +74,54 @@ D;JGE    // If arr[i+1] >= arr[i], skip swap
 // Swap arr[i] and arr[i+1]
 @R1
 D=M
-@i
+@innerCounter
 A=D+M    // Address of arr[i]
-D=M      // D = arr[i]
-@temp
-M=D      // temp = arr[i]
+D=A
+@addr1
+M=D      // Store address of arr[i]
 
 @R1
 D=M
-@i
+@innerCounter
 D=D+M
 A=D+1    // Address of arr[i+1]
-D=M      // D = arr[i+1]
-@R1
-A=M
-@i
-A=D+M    // Address of arr[i]
-M=D      // arr[i] = arr[i+1]
+D=A
+@addr2
+M=D      // Store address of arr[i+1]
 
-@temp
-D=M      // D = temp (original arr[i])
-@R1
+@temp1
+D=M
+@addr2
 A=M
-@i
-D=D+M
-A=D+1    // Address of arr[i+1]
-M=D      // arr[i+1] = temp
+M=D      // arr[i+1] = temp1 (original arr[i])
+
+@temp2
+D=M
+@addr1
+A=M
+M=D      // arr[i] = temp2 (original arr[i+1])
 
 @swapped
 M=-1     // Set swapped flag to true (-1)
 
 (SKIP_SWAP)
-@i
-M=M+1    // i++
+@innerCounter
+M=M+1    // innerCounter++
 @INNER_LOOP
 0;JMP
 
 (OUTER_LOOP_END)
 @swapped
 D=M
-@OUTER_LOOP
+@OUTER_LOOP_CONTINUE
 D;JNE    // If swapped is true (-1), continue outer loop
 
-@n
-M=M-1    // n--
+@END
+0;JMP    // If no swaps were made, the array is sorted
+
+(OUTER_LOOP_CONTINUE)
+@outerCounter
+M=M+1    // outerCounter++
 @OUTER_LOOP
 0;JMP
 
