620c4081adce3cc94dd872305c8dc7915d713c1f
HoffmanEl <elijah.hoffman64@gmail.com>
2024-09-14 15:45:48 +0930

    second

--- prac5/ArrMin/ArrMin.asm
+++ prac5/ArrMin/ArrMin.asm
@@ -1,84 +1,51 @@
-// Finds the smallest element in the array of length R2 whose first element is at RAM[R1] and stores the result in R0.
-// (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
+// Initialize R0 with the first element of the array
+@1
+D=M      // D = RAM[1] (start address of the array)
+A=D      // A = address of the first element
+D=M      // D = value of the first element
+@0
+M=D      // R0 = first element (initial minimum)
 
-// Put your code here.
-// Find the smallest element in the array and store it in R0
-
-// Initialize R0 to the first element of the array
-@1       // Address of RAM[1] (R1)
-D=M      // D = RAM[1] (first element of the array)
-@0       // Address of RAM[0] (R0)
-M=D      // Store the first element in R0 as the initial minimum
-
-// Set up loop counter
-@2       // Address of RAM[2] (R2)
+// Initialize loop counter
+@2
 D=M      // D = RAM[2] (length of the array)
 @COUNTER
-M=D      // Store length in COUNTER
+M=D      // COUNTER = length of the array
 
-// Set up array index
-@1       // Address of RAM[1] (R1)
+// Initialize array index
+@1
 D=M      // D = RAM[1] (start address of the array)
 @INDEX
-M=D      // Store the start address in INDEX
+M=D      // INDEX = start address of the array
 
-// Start loop
 (LOOP)
 @COUNTER
-D=M      // D = COUNTER (remaining elements to check)
+D=M
 @END
-D;JEQ    // If COUNTER == 0, jump to END
+D;JLE    // If COUNTER <= 0, end the loop
 
 @INDEX
-D=M      // D = INDEX (current element address)
-A=D      // A = address of the current element
-D=M      // D = value of the current element
-
-@0       // Address of RAM[0] (R0)
-D=D-M    // D = value of the current element - current min
-@SMALLER
-D;JLT    // If value of the current element < current min, jump to SMALLER
+A=M      // A = address of current element
+D=M      // D = value of current element
+@0
+D=D-M    // D = current element - current minimum
+@SKIP
+D;JGE    // If D >= 0, skip to next element
 
 @INDEX
-D=M      // D = INDEX (current element address)
-@1       // Address of RAM[1] (R1)
-D=D+1    // Increment INDEX to the next element
-@INDEX
-M=D      // Store the new INDEX value
+A=M
+D=M
+@0
+M=D      // Update R0 with new minimum
 
+(SKIP)
+@INDEX
+M=M+1    // Move to next element
 @COUNTER
-D=M      // D = COUNTER
-@1
-D=D-1    // Decrement COUNTER
-@COUNTER
-M=D      // Store the new COUNTER value
-
+M=M-1    // Decrement COUNTER
 @LOOP
-0;JMP    // Jump to start of the loop
+0;JMP
 
-// Update minimum
-(SMALLER)
-@0       // Address of RAM[0] (R0)
-M=D      // Store the new minimum in R0
-
-@INDEX
-D=M      // D = INDEX (current element address)
-@1       // Address of RAM[1] (R1)
-D=D+1    // Increment INDEX to the next element
-@INDEX
-M=D      // Store the new INDEX value
-
-@COUNTER
-D=M      // D = COUNTER
-@1
-D=D-1    // Decrement COUNTER
-@COUNTER
-M=D      // Store the new COUNTER value
-
-@LOOP
-0;JMP    // Jump to start of the loop
-
-// End of the program
 (END)
 @END
-0;JMP    // Infinite loop to end the program
+0;JMP
