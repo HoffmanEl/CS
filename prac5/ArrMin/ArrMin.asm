6fe5f37be1364abc9ad8ca96b4551d2546632530
HoffmanEl <elijah.hoffman64@gmail.com>
2024-09-15 15:50:45 +0930

    working everythign

--- prac5/ArrMin/ArrMin.asm
+++ prac5/ArrMin/ArrMin.asm
@@ -1,64 +1,69 @@
-// Initialize R0 with the first element of the array
-@1
-D=M        // D = RAM[1] (start address of the array)
-@INDEX
-M=D        // Store start address in INDEX
+  @R1
+    D=M-1
+    @R2
+    M=M+D
 
-@2
-D=M        // D = RAM[2] (length of the array)
-@COUNTER
-M=D        // COUNTER = length of the array
-
-// Check if array is empty
-@COUNTER
-D=M
-@END
-D;JLE      // If length <= 0, end the loop
-
-// Load first element as initial minimum
-@INDEX
-A=M        // A = address of first element
-D=M        // D = value of first element
-@0
-M=D        // R0 = first element (initial minimum)
+    @32767
+    D=A
+    @R0
+    M=D
 
 (LOOP)
-@COUNTER
-D=M
-@END
-D;JLE      // If COUNTER <= 0, end the loop
-
-@INDEX
-A=M        // A = address of current element
-D=M        // D = value of current element
-
-// Compare current element with current minimum
-@0
-D=M-D      // D = current minimum - current element
-
-// Check for overflow and valid comparison
-@UPDATE_MIN
-D;JGT      // If current minimum > current element, update minimum
-
-// Continue without updating
-@SKIP
-0;JMP      
-
-(UPDATE_MIN)
-@INDEX
-A=M
-D=M        // Load current element again
-@0
-M=D        // Update R0 with new minimum
-
+(CHECK_TERMINATE)
+	@R1
+	D=M
+	@R2
+	D=D-M
+	@END
+	D;JGT
+	@R1
+	A=M
+	D=M
+    @ELEM_POS
+    D;JGE
+	@ELEM_NEG
+	0;JMP
+(UPDATE)
+	@R1
+	A=M
+	D=M
+	@R0
+	M=D
 (SKIP)
-@INDEX
-M=M+1      // Move to next element
-@COUNTER
-M=M-1      // Decrement COUNTER
-@LOOP
-0;JMP      // Continue looping
-
+	@R1
+	M=M+1
+	@LOOP
+	0;JMP
 (END)
-@END
-0;JMP      // End program
+	@END
+	0;JMP
+
+(R0_NEG)
+    
+(R0_POS)
+    // subs
+    @R1
+	A=M
+	D=M
+	@R0
+	D=D-M // substraction, may cause Overflow!
+    @SKIP
+	D;JGE
+    @UPDATE
+    0;JMP
+
+(ELEM_NEG)
+    @R0
+    D=M
+    @R0_NEG
+    D;JLT
+    @UPDATE
+    0;JMP
+
+(ELEM_POS)
+    @R0
+    D=M
+    @R0_POS
+    D;JGE
+    @SKIP
+    0;JMP
